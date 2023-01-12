91354405d87854b2d55f728579118fd5eab92af5
236547 Janicki Marcin <236547@edu.p.lodz.pl>
2023-01-12 15:27:27 +0100

    Jdbc test?

--- Model/src/main/java/dao/JdbcSudokuBoardDao.java
+++ Model/src/main/java/dao/JdbcSudokuBoardDao.java
@@ -1,12 +1,17 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import model.BacktrackingSudokuSolver;
 import model.SudokuBoard;
 import model.exceptions.JdbcDatabaseException;
 
-import java.sql.*;
 
-public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable{
+public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable {
 
     private static final String URL = "jdbc:postgresql://localhost/jdbcSudoku";
     private static final String USER = "postgres";
@@ -17,10 +22,10 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable{
         this.boardName = boardName;
     }
 
-    private Connection connect() throws JdbcDatabaseException{
+    private Connection connect() throws JdbcDatabaseException {
         Connection connection;
         try {
-            connection = DriverManager.getConnection(URL,USER,PASSWORD);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
             throw new JdbcDatabaseException("connectionError", e);
         }
@@ -37,7 +42,7 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable{
         String select = "select tableName, fields from sudokuboards_table where tableName=?";
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(select)) {
-            preparedStatement.setString(1,boardName);
+            preparedStatement.setString(1, boardName);
             resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
                 receivedData = resultSet.getString(2);
@@ -67,8 +72,8 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable{
         String insert = "INSERT INTO `sudokuproject`.`"
                 + "sudokuboards_table`(`tableName`,`fields`) VALUES (?,?);";
         try (PreparedStatement preparedStatement = connection.prepareStatement(insert)) {
-            preparedStatement.setString(1,boardName);
-            preparedStatement.setString(2,boardValues.toString());
+            preparedStatement.setString(1, boardName);
+            preparedStatement.setString(2, boardValues.toString());
             preparedStatement.executeUpdate();
         } catch (SQLException | NullPointerException e) {
             throw new JdbcDatabaseException("Write error", e);
@@ -82,7 +87,7 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable{
 
     static void createSudokuBoardsTable() throws JdbcDatabaseException {
         String sudokuBoards = "Create Table SudokuBoards(id_board int primary key, name text) ";
-        try (var con = DriverManager.getConnection(URL,USER,PASSWORD)) {
+        try (var con = DriverManager.getConnection(URL, USER, PASSWORD)) {
             Statement statement = con.createStatement();
             statement.execute(sudokuBoards);
         } catch (SQLException e) {
@@ -91,17 +96,14 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable{
     }
 
     private static void createSudokuValuesTable() throws JdbcDatabaseException {
-        String sudokuValues = "Create Table SudokuValues(id_value int primary key, value int, id_board int, CONSTRAINT board_FK FOREIGN KEY (id_board) REFERENCES SudokuBoards(id_board)) ";
-        try (var con = DriverManager.getConnection(URL,USER,PASSWORD)) {
+        String sudokuValues = "Create Table SudokuValues(id_value int primary key, "
+                + "value int, id_board int," + " CONSTRAINT board_FK FOREIGN KEY (id_board) "
+                + "REFERENCES SudokuBoards(id_board)) ";
+        try (var con = DriverManager.getConnection(URL, USER, PASSWORD)) {
             Statement statement = con.createStatement();
             statement.execute(sudokuValues);
         } catch (SQLException e) {
             throw new JdbcDatabaseException("Cannot create table error", e);
         }
     }
-
-//    public ResultSet executeSelect(){
-//
-//    }
-
 }
--- Model/src/test/java/dao/JdbcSudokuBoardDaoTest.java
+++ Model/src/test/java/dao/JdbcSudokuBoardDaoTest.java
@@ -1,19 +1,75 @@
 package dao;
 
+import model.exceptions.DaoException;
+import model.exceptions.JdbcDatabaseException;
+import org.apache.commons.lang3.SystemUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import model.exceptions.FileDaoException;
 import model.BacktrackingSudokuSolver;
 import model.SudokuBoard;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static dao.JdbcSudokuBoardDao.createSudokuBoardsTable;
 import static org.junit.jupiter.api.Assertions.*;
 
-class JdbcSudokuBoardDaoTest {
+class JdbcSudkouBoardDaoTest {
 
     @Test
-    void addtabletest() throws IOException {
-        createSudokuBoardsTable();
+    void daoReadSaveDatabaseTest() throws DaoException {
+        SudokuBoard board1 = new SudokuBoard(new BacktrackingSudokuSolver());
+        board1.solveGame();
+        SudokuBoardDaoFactory factory = new SudokuBoardDaoFactory();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        String name = dtf.format(now).toString();
+        factory.getDatabaseDao(name).write(board1);
+        SudokuBoard board2 = factory.getDatabaseDao(name).read();
+        assertEquals(board1, board2);
     }
 
+    @Test
+    void wrongInputTest() throws DaoException {
+        JdbcDatabaseException thrown = Assertions.assertThrows(JdbcDatabaseException.class, () -> {
+            SudokuBoardDaoFactory factory = new SudokuBoardDaoFactory();
+            ;
+            if (SystemUtils.IS_OS_WINDOWS) {
+                factory.getDatabaseDao("?").read();
+            } else if (SystemUtils.IS_OS_LINUX) {
+                factory.getDatabaseDao("/").read();
+            } else {
+                factory.getDatabaseDao("?").read();
+            }
+        });
+    }
+
+    @Test
+    void databaseAlreadyExistsWriteScenario() throws DaoException {
+        JdbcDatabaseException thrown = Assertions.assertThrows(JdbcDatabaseException.class, () -> {
+            SudokuBoard board2 = new SudokuBoard(new BacktrackingSudokuSolver());
+            board2.solveGame();
+            SudokuBoardDaoFactory factory = new SudokuBoardDaoFactory();
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            String name = dtf.format(now).toString();
+            name += "z";
+            factory.getDatabaseDao(name).write(board2);
+            factory.getDatabaseDao(name).write(board2);
+        });
+    }
+
+    @Test
+    void databaseDoesNotExistsReadScenario() throws DaoException {
+        JdbcDatabaseException thrown = Assertions.assertThrows(JdbcDatabaseException.class, () -> {
+            SudokuBoard board1 = new SudokuBoard(new BacktrackingSudokuSolver());
+            SudokuBoardDaoFactory factory = new SudokuBoardDaoFactory();
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            String name = dtf.format(now).toString();
+            name += "zz";
+            board1 = factory.getDatabaseDao(name).read();
+        });
+    }
 }
